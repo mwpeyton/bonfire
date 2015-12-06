56899c9dee65349e042cb2c6a5f9287c2173fb2a
Mike <mikewpeyton@gmail.com>
2015-12-05 20:21:55 -0600

    set up initial server configuration

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+var Hapi = require('hapi');
 
+var server = new Hapi.Server();
+
+server.connection({ port: 8080 });
+
+server.start(function(){
+	console.log('server running on ' + server.info.uri);
+});
