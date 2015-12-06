e65de9635e2eb66312b1e23c0c171518a02840a9
Mike <mikewpeyton@gmail.com>
2015-12-05 20:32:48 -0600

    made additional config changes to server

--- server.js
+++ server.js
@@ -4,6 +4,46 @@ var server = new Hapi.Server();
 
 server.connection({ port: 8080 });
 
+// server.ext('onPreResponse', function(request, reply){
+// 	if(request.response.isBoom){
+// 		return reply.view('error', request.response);
+// 	}
+
+// 	reply.continue();
+// });
+
+server.register(require('inert'), function(err){
+	if(err)throw err;
+
+	server.route(require('./lib/inert_routes'));
+});
+
+server.register(require('vision'), function(err){
+	if(err)throw err;
+
+	server.views({
+		engines: {
+			html: require('handlebars')
+		},
+		relativeTo: __dirname,
+		path: 'templates'
+	});
+
+	server.route(require('./lib/vision_routes'));
+});
+
+// server.register(require('hapi-auth-cookie'), function(err){
+// 	if(err)throw err;
+
+// 	server.auth.strategy('default', 'cookie', {
+// 		password: 'myPassword',
+// 		redirectTo: '/login',
+// 		isSecure: false
+// 	});
+
+// 	server.auth.default('default');
+// });
+
 server.start(function(){
 	console.log('server running on ' + server.info.uri);
 });
